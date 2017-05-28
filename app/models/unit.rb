cc7bcd45cdfcbf00718d5fb5ed348a004b8536e2
nullwolf <nullwolf>
2017-05-28 20:20:18 +1000

    removing unused methods from unit. they were for manchester. I really hope they werent in use. I took a look but time will tell

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,33 +2,4 @@ class Unit < ActiveRecord::Base
   belongs_to :device
   has_many :captures, dependent: :destroy
 
-
-  def check_signals
-    manchester_failures = {}
-    captures.each do |cap|
-      cap.original_binary.scan(/../).each do |pair|
-        manchester_failures[pair]='invalid manchester' if (pair != "10") and (pair != "01")
-      end
-    end
-
-    manchester_failures
-  end
-
-
-  def valid_manchester?
-    check_signals.blank?
-  end
-
-  def manchester_decode
-    symbols = ""
-    captures.each do |cap|
-      cap.original_binary.scan(/../).each do |s|
-        puts s.to_i == 10
-        symbols += '1' if s.to_i == 10
-        symbols += '0' if s.to_i == 01
-      end
-    end
-    symbols
-  end
-
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Section, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+	describe "Associations" do
+		it { should belong_to(:device) }
+	end
+
+	describe "section range" do 
+	  	subject { described_class.create(name:'section name', start_pos: 0, end_pos: 16) }
+		it "correctly returns range object" do
+			expect(subject.section_range).to eq(0..16)
+		end
+	end
 end
